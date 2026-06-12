ef74478ed8acf4fd3b91643384be9d0b06920ca7
Depiliem <davew1726@gmail.com>
2026-06-12 19:48:17 +0700

    tele ga masuk tanah, ui fungsional
    asdasda

--- Barista_VR/Assets/Script/FloatingUIManager.cs
+++ Barista_VR/Assets/Script/FloatingUIManager.cs
@@ -1,14 +1,60 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class FloatingUIManager : MonoBehaviour
 {
     public Transform targetCamera;
-    public Transform uiTransform;       
+    public Transform uiTransform;
     public float followDistance = 3.5f;
     public float followSpeed = 5f;
     public float heightOffset = 0f;
     public GameObject menuUIPanel;
+
+    public Slider volumeSlider;
+    public Slider vignetteSlider;
+    public Toggle teleportToggle;
+    public Toggle snapTurnToggle;
+
+    public Behaviour teleportationProvider;
+    public Behaviour snapTurnProvider;
+
+    public Volume globalVolume;
+    private Vignette vignetteProfile;
+
+    private void Start()
+    {
+        if (globalVolume != null)
+        {
+            globalVolume.profile.TryGet(out vignetteProfile);
+        }
+
+        if (volumeSlider != null)
+        {
+            volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
+            OnVolumeChanged(volumeSlider.value);
+        }
+        
+        if (vignetteSlider != null)
+        {
+            vignetteSlider.onValueChanged.AddListener(OnVignetteChanged);
+            OnVignetteChanged(vignetteSlider.value);
+        }
+
+        if (teleportToggle != null)
+        {
+            teleportToggle.onValueChanged.AddListener(OnTeleportToggled);
+            if (teleportationProvider != null) teleportationProvider.enabled = teleportToggle.isOn;
+        }
+
+        if (snapTurnToggle != null)
+        {
+            snapTurnToggle.onValueChanged.AddListener(OnSnapTurnToggled);
+            if (snapTurnProvider != null) snapTurnProvider.enabled = snapTurnToggle.isOn;
+        }
+    }
 
     private void Update()
     {
@@ -33,5 +79,34 @@ public class FloatingUIManager : MonoBehaviour
         Vector3 lookAtPosition = new Vector3(targetCamera.position.x, uiTransform.position.y, targetCamera.position.z);
         uiTransform.LookAt(lookAtPosition);
         uiTransform.Rotate(0, 180, 0);
+    }
+
+    private void OnVolumeChanged(float value)
+    {
+        AudioListener.volume = value;
+    }
+
+    private void OnVignetteChanged(float value)
+    {
+        if (vignetteProfile != null)
+        {
+            vignetteProfile.intensity.value = value;
+        }
+    }
+
+    private void OnTeleportToggled(bool isOn)
+    {
+        if (teleportationProvider != null)
+        {
+            teleportationProvider.enabled = isOn;
+        }
+    }
+
+    private void OnSnapTurnToggled(bool isOn)
+    {
+        if (snapTurnProvider != null)
+        {
+            snapTurnProvider.enabled = isOn;
+        }
     }
 }

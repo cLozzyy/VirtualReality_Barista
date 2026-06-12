43c2f1684e2520be3937e5282a5cdadebafc2811
panda5201 <vinsongautama78@gmail.com>
2026-06-12 18:39:18 +0700

    Floating

---
+++ Barista_VR/Assets/Script/ToggleInteractor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors; // ← tambah ini
+
+public class ToggleInteractor : MonoBehaviour
+{
+    [Header("Tangan Kanan")]
+    public XRDirectInteractor nearInteractorKanan;
+    public XRRayInteractor farInteractorKanan;
+
+    [Header("Tangan Kiri")]
+    public XRDirectInteractor nearInteractorKiri;
+    public XRRayInteractor farInteractorKiri;
+
+    void Update()
+    {
+        if (Keyboard.current.fKey.wasPressedThisFrame)  // ← ganti ini
+        {
+            bool nearAktif = nearInteractorKanan.enabled;
+
+            nearInteractorKanan.enabled = !nearAktif;
+            farInteractorKanan.enabled = nearAktif;
+
+            nearInteractorKiri.enabled = !nearAktif;
+            farInteractorKiri.enabled = nearAktif;
+        }
+    }
+}
